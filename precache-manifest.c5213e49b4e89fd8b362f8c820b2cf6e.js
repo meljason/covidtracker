2488d89da75f8ef3fcf075b0241106a4f14e3442
Jason Chong <34180725+meljason@users.noreply.github.com>
2020-05-09 02:42:22 -0400

    Updates

---
+++ precache-manifest.c5213e49b4e89fd8b362f8c820b2cf6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5085c63b1ce310c35994ad18423b77",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "26ff510e785c627a2eb3",
+    "url": "/covidtracker/static/css/main.4b870c3b.chunk.css"
+  },
+  {
+    "revision": "f6d410a5d758177778f2",
+    "url": "/covidtracker/static/js/2.7767a7e8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.7767a7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ff510e785c627a2eb3",
+    "url": "/covidtracker/static/js/main.1785aef2.chunk.js"
+  },
+  {
+    "revision": "bf94e0a017409e884242",
+    "url": "/covidtracker/static/js/runtime-main.f24466a6.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
